1a54eac1bd106f49a29070ad6e2c444b950af064
Richard <ricky.cary1@gmail.com>
2023-01-21 13:51:23 -0600

    Updated app and router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 
 import './App.css';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
-    null
+    <div className='App'>
+      <Header />
+      <Outlet />
+    </div>
     
   );
 }
