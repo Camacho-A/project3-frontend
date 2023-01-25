2f793b89b5dc501984b95932c90e74413f979763
Michelle Chen <chensiqi96@gmail.com>
2023-01-24 16:39:44 -0800

    adds show page with show, update, delete functionality and updated main.js with new artist creation form

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,17 +6,20 @@ function Main(props){
     return (
         <div>
             <h2>Create a Profile</h2>
-            <Form action ="/create" method="post">
-                <input type="input" name="profile" placeholder="Profile's Name" />
-                <input type="input" name="image" placeholder="Profile's Image" />
-                <input type="submit" value="Create a Profile" />
+            <Form action={`/create`} method="post">
+                <input type="input" name="artist" placeholder="artist name"/>
+                <input type="input" name="genre" placeholder="genre"/>
+                <input type="input" name="song" placeholder="song"/>
+                <input type="input" name="url" placeholder="song url"/>
+                <input type="input" name="albumImage" placeholder="album image"/>
+                <input type="submit" value={`Add an Artist`}/>
             </Form>
 
-            <h2>Profiles</h2>
+            <h2>Artists</h2>
             {musics.map(music => (
                 <div key={music._id} className="music">
                     <Link to={`/${music._id}`}>
-                        <h1>{music.profile}</h1>
+                        <img src={music.albumImage} alt={music.song}/>
                     </Link>
                 </div>
             ))}
--- src/router.js
+++ src/router.js
@@ -7,15 +7,15 @@ import App from "./App"
 import Main from "./pages/Main"
 import Show from "./pages/Show"
 import { musicsLoader, musicLoader } from "./loader"
-import { createAction } from "./action"
+import { createAction,updateAction,deleteAction } from "./action"
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<App />}>
         <Route path="" element={<Main />} loader={musicsLoader} />
         <Route path=":id" element={<Show />} loader={musicLoader} />
         <Route path="create" action={createAction}/>
-        <Route path="update/:id" />
-        <Route path="delete/:id" />
+        <Route path="update/:id" action={updateAction} />
+        <Route path="delete/:id" action={deleteAction} />
     </Route>
 ))
 
